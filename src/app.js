c7c57917cd99981e227055c3740a9f51760c11e9
HecOmAC <malinchecoatl@gmail.com>
2026-03-05 05:16:58 +0000

    diseno de la logica

--- src/app.js
+++ src/app.js
@@ -8,4 +8,27 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+let palos = ['Corazones', 'Picas', 'Trebol', 'Diamantes']
+let valor = ['A', '2', '3', '4','5','6','7','8','9','10', 'R', 'Rna', 'J'];
+
+function generadorDeCartas(){
+
+let aleatorio = Math.floor(Math.random() * palos.length);
+let aleatorio2 = Math.floor(Math.random() * valor.length);
+
+
+return (palos[aleatorio]+' '+valor[aleatorio2]);
+
+
+};
+
+generadorDeCartas();
+
+let carta = generadorDeCartas();
+document.getElementById("carta").innerHTML = carta;
+console.log(carta);
+
+
+
 };
